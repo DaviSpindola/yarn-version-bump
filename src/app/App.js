be10be9e76d6f51f85839cbd62550d252092495c
Davi Spindola <davispindola2828@gmail.com>
2021-07-21 22:15:04 -0300

    move entry file to app folder

---
+++ src/app/App.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+function App({ logo, version }) {
+  return (
+    <div className="container">
+      <div className="content">
+        <img src={logo} aria-label="Yarn logo" className="image--sm"/>
+        <h1 className="h1 center">Yarn Version</h1>
+      </div>
+      <footer>
+        <span>app version {version}</span>
+      </footer>
+    </div>
+  );
+}
+
+App.propTypes = {
+  logo: PropTypes.string.isRequired,
+  version: PropTypes.string.isRequired,
+}
+
+export default App;
